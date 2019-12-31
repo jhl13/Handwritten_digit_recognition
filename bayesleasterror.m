cbcf0f95223835c389525e9779170d499e4dad75
luo13 <1249954696@qq.com>
2019-12-31 19:57:23 +0800

    [update]最小错误率准确率0.88

--- bayesleasterror.m
+++ bayesleasterror.m
@@ -4,31 +4,31 @@ function [result,v]=BayesLeasterror(testfeature, template_feature, template_num)
 % template_num = (10, 1)
 
 Pw = zeros(10, 1); % 先验概率
-P = zeros(10, 100); % wi类的 j个特征的均值
+P = zeros(10, 25); % wi类的 j个特征的均值
 PwX = zeros(10, 1); % 后验概率
 
-s_cov = []; %各类别协方差 (10, 100, 100)
-s_inv = []; %协方差矩阵的逆 (10, 100, 100)
-s_det = []; %协方差矩阵的行列式 (10, 100, 100)
+s_cov = double([]); %各类别协方差 (10, 100, 100)
+s_inv = double([]); %协方差矩阵的逆 (10, 100, 100)
+s_det = double([]); %协方差矩阵的行列式 (10, 100, 100)
 
 [total_num, ~] = size(template_feature);
 Pw = template_num / total_num;
 
 for i=1:10
-    for j=2:101
+    for j=2:26
         class_index = template_feature(:, 1) == i-1;
-        numof1 = sum(template_feature(class_index,j)==1);
-        P(i,j) = (numof1+1)/(template_num(i)+2);
+        numof1 = sum(template_feature(class_index,j));
+        P(i,j) = (numof1)/(template_num(i));
     end
     i_feature = template_feature(template_feature(:, 1) == i-1,:);
-    s_cov(i).dat = cov(i_feature(:,2:101)); % 求各类别的协方差矩阵
+    s_cov(i).dat = cov(i_feature(:,2:26)); % 求各类别的协方差矩阵
     s_inv(i).dat = pinv(s_cov(i).dat); % 求协方差矩阵的逆矩阵
     s_det(i) = det(s_cov(i).dat); % 求协方差矩阵的行列式
 end
 
 for i=1:10
-    PwX(i) = (testfeature - P(i, 2:101)')' * s_inv(i).dat * (testfeature - P(i, 2:101)')...
-        * (-0.5) + log(Pw(i));% + log(abs(s_det(i))) * (-0.5)
+    PwX(i) = (testfeature - P(i, 2:26)')' * s_inv(i).dat * (testfeature - P(i, 2:26)')...
+        * (-0.5) + log(abs(s_det(i))) * (-0.5);
 end
 [v,result]=max(PwX);
 result=result-1;
--- getfeature.m
+++ getfeature.m
@@ -3,20 +3,20 @@ function [feature,featureimg]=getfeature(image, is_float)
             is_float = 0;
     end
 %% 找出数字区域
-[ax,by] = find(image<=100);
+[ax,by] = find(image==0);
 image = image(min(ax):max(ax),min(by):max(by));
-image = imresize(image, [50, 50]);
+image = imresize(image, [30, 30]);
 %% 计算格点
 [N,M] = size(image);
-row = floor(N/10);
-col = floor(M/10);
+row = floor(N/5);
+col = floor(M/5);
 area = row*col;
-sign = zeros(10,10);
-feature = zeros(100,1);
+sign = zeros(5,5);
+feature = zeros(25,1);
 k=0;
 if is_float == 0
-    for i=1:1:10
-        for j=1:1:10
+    for i=1:1:5
+        for j=1:1:5
             k=k+1;
             sign(i,j)=(length(find(image(1+(i-1)*row:i*row,1+(j-1)*col:j*col)<=100))/area>0.1);
             feature(k)=~sign(i,j);
@@ -24,11 +24,11 @@ if is_float == 0
     end
     featureimg=~sign;
 else
-    for i=1:1:10
-        for j=1:1:10
+    for i=1:1:5
+        for j=1:1:5
             k=k+1;
-            sign(i,j)=(length(find(image(1+(i-1)*row:i*row,1+(j-1)*col:j*col)<=160))/area);
-            feature(k)=1 - sign(i,j);
+            sign(i,j)=(length(find(image(1+(i-1)*row:i*row,1+(j-1)*col:j*col)==0)/area));
+            feature(k)=sign(i,j);
         end
     end
     %featureimg=1 - sign;
